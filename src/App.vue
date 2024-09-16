4a57c23b500de11f576bcaeb83db1f4d3aa5dbd2
Kevin Koontz <contactkkoontz@gmail.com>
2024-09-16 16:26:09 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+// JS
+<script setup>
+import { computed } from 'vue'
+import { AppState } from './AppState.js'
+
+
+</script>
+
+
+// HTML
+<template>
+
+</template>
+
+
+// styles
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+</style>
