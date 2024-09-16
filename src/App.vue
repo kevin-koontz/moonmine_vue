087a2dfd7cf79bd520b78c9838eb0d71145fca22
Kevin Koontz <contactkkoontz@gmail.com>
2024-09-16 16:54:00 -0600

    updated script + template to create button => cheese++ to test

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,26 @@
 <script setup>
 import { computed } from 'vue'
 import { AppState } from './AppState.js'
+import { ref } from "vue";
 
+const cheese = ref(0)
 
 </script>
 
 
 // HTML
 <template>
-
+  <main>
+    <div class="container">
+      <section class="row">
+        <div class="col-12">
+          <div>
+            <button @click="cheese++" class="btn btn-primary my-3">{{ cheese }} CHEESE</button>
+          </div>
+        </div>
+      </section>
+    </div>
+  </main>
 </template>
 
 
